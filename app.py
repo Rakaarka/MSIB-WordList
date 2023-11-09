5ec88fdac8fc6e340363809ca09a168363c8ffb0
Rakaarka <rakamuhammad644@gmail.com>
2023-11-09 17:47:27 +0700

    Flask Set Up For Deployment

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import (
     Flask,
     request,
@@ -11,11 +14,17 @@ import requests
 from datetime import datetime
 from bson import ObjectId
 
-app = Flask(__name__)
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
 
-cxn_str = 'mongodb+srv://test:sparta@cluster0.qmkvztt.mongodb.net/?retryWrites=true&w=majority&appName=AtlasApp'
-client = MongoClient(cxn_str)
-db = client.dbsparta_plus_week2
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME = os.environ.get("DB_NAME")
+
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
+app = Flask(__name__)
 
 
 @app.route("/")
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\KULIAH\Semester 5\MSIB\Chapter 10\Project 02\venv
+set VIRTUAL_ENV=D:\KULIAH\Semester 5\MSIB\Chapter 11\Prjct-2\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
